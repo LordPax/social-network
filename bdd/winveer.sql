4f788034b147ead9bc9e8107691954eb651c4d90
LordPompax <capitaine_ted@hotmail.fr>
2020-01-07 22:16:05 +0100

    update : winveer.sql

--- bdd/winveer.sql
+++ bdd/winveer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  Dim 05 jan. 2020 à 21:29
+-- Généré le :  mar. 07 jan. 2020 à 22:12
 -- Version du serveur :  10.3.16-MariaDB
 -- Version de PHP :  7.3.6
 
@@ -32,7 +32,7 @@ CREATE TABLE `reponse` (
   `id` int(11) NOT NULL,
   `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `id_post` varchar(255) NOT NULL,
-  `id_user` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `id_user` int(11) NOT NULL DEFAULT 0,
   `date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -47,7 +47,7 @@ CREATE TABLE `thread` (
   `str_id` varchar(255) NOT NULL,
   `title` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `user` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `user` int(11) NOT NULL DEFAULT 0,
   `date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -64,6 +64,14 @@ CREATE TABLE `user` (
   `email` varchar(255) NOT NULL,
   `rank` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `rank`) VALUES
+(0, 'noname', 'none', 'none', 0),
+(1, 'LordPax', 'c4d4bb8b223063a5de2a7609d084205c5c94339dba9aa1e88f377e7b0f430c7b', 'capitaine_ted@hotmail.fr', 1);
 
 --
 -- Index pour les tables déchargées
@@ -107,7 +115,7 @@ ALTER TABLE `thread`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
